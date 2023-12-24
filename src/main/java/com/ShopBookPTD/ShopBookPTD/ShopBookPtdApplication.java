796479456804500f248f380e9a489ca31b4f4b16
phamtranduc <phamtranducc@gmail.com>
2023-12-24 21:22:36 +0700

    init project

---
+++ src/main/java/com/ShopBookPTD/ShopBookPTD/ShopBookPtdApplication.java
@@ -0,0 +1,13 @@
+package com.ShopBookPTD.ShopBookPTD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBookPtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBookPtdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ShopBookPTD/ShopBookPTD/ShopBookPtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShopBookPTD.ShopBookPTD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBookPtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

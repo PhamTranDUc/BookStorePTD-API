5af9afb96c3b3bb20965eebb7b49d9e6b82e033e
phamtranduc <phamtranducc@gmail.com>
2023-12-30 20:58:18 +0700

    add main Controller

---
+++ src/main/java/com/BookStorePTD/BookStorePTD/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.BookStorePTD.BookStorePTD.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+}

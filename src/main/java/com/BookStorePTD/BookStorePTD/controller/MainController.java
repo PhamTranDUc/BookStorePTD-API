284cebf6f9bde9c758440096c5eb76c3d4414ca5
phamtranduc <phamtranducc@gmail.com>
2023-12-31 17:58:31 +0700

    init project

--- src/main/java/com/BookStorePTD/BookStorePTD/controller/MainController.java
+++ src/main/java/com/BookStorePTD/BookStorePTD/controller/MainController.java
@@ -1,14 +1,12 @@
 package com.BookStorePTD.BookStorePTD.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String getIndex(){
         return "index";
     }
